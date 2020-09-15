453ec657fae74566882fb558c5fb0573389ddd36
sadat <sadat.himel@gmail.com>
2020-09-15 21:05:00 +0600

    thumb up icon set with material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  // const handleLike = () => {
+  //   // const color = likeColor ? '' : 'primary';
+  //   // setLikeColor(likeColor ? "" : "primary");
+  // }
   return (
     <div className="App">
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpAltIcon onClick={()=> setLikeColor(likeColor ? "" : "primary")} color={likeColor}></ThumbUpAltIcon>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
